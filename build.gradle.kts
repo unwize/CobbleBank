1b1febd09842520013ac9e181ad5adce42a2d7de
Unwize <asigal55+02@gmail.com>
2025-01-14 12:58:41 -0800

    Added deps, configured gradle, refactored main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,18 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "com.unwize"
 version = "1.0-SNAPSHOT"
+
+
+
+application {
+    mainClass = "com.unwize.cobblebank.Main"
+}
+
+
 
 repositories {
     mavenCentral()
@@ -12,6 +21,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.javalin:javalin:6.4.0")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+    implementation("com.alibaba:fastjson:2.0.54")
 }
 
 tasks.test {
---
+++ src/main/java/com/unwize/cobblebank/Main.java
@@ -0,0 +1,11 @@
+package com.unwize.cobblebank;
+
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        var app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
+    }
+}
